4d0ffffee07ab133295cd34999f3251297878d71
ramshell688 <ramonzost68@gmail.com>
2023-11-14 18:49:53 +0700

    camellia: SuperiorExtended bring up!
    Signed-off-by: ramshell688 <ramonzost68@gmail.com>

--- superior_camellia.mk
+++ superior_camellia.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := aosp_camellia
+PRODUCT_NAME := superior_camellia
 PRODUCT_DEVICE := camellia
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
